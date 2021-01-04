22d86b586a896a4086df309154df5475cedefef1
박정현 <jh@bagjeoncBookAir.Dlink>
2021-01-04 20:26:22 +0900

    End~

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const canvas = document.getElementById("jsCanvas");
 const color = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
 const ctx = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 500;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 500, 500);
 ctx.strokeStyle = '#2c2c2c';
+ctx.fillStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let isPainting = false;
@@ -34,6 +38,7 @@ function onMouseMove(event) {
 
 function changeColor(event) {
     ctx.strokeStyle = event.target.style.backgroundColor;
+    ctx.fillStyle = ctx.strokeStyle;
 }
 
 function changeRange(event) {
@@ -41,13 +46,24 @@ function changeRange(event) {
 }
 
 function changeMode(evnet) {
-    if(isFill){
+    if (isFill) {
         isFill = false;
         mode.innerText = "Fill";
-    }else{
+    } else {
         isFill = true;
         mode.innerText = "Paint";
     }
+}
+
+function onMouseClick() {
+    if (isFill) {
+        ctx.fillRect(0, 0, 500, 500);
+    }
+}
+
+function onMouseRightClick(event) {
+    console.log(event);
+    event.preventDefault();
 }
 
 if (canvas.getContext) {
@@ -55,6 +71,8 @@ if (canvas.getContext) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", onMouseClick);
+    canvas.addEventListener("contextmenu", onMouseRightClick);
 }
 
 if (range) {
@@ -63,6 +81,16 @@ if (range) {
 
 if (mode) {
     mode.addEventListener("click", changeMode);
+}
+
+if (save) {
+    save.addEventListener("click", e => {
+        const imageURL = canvas.toDataURL();
+        const link = document.createElement("a");
+        link.href = imageURL;
+        link.download = "hello.png";
+        link.click();
+    });
 }
 
 Array.from(color).forEach(color => color.addEventListener("click", changeColor));
